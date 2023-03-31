731cd8b37360c4f60250d4cd8c7faa335d99e9a2
dipak <sarkar3115@gmail.com>
2023-03-31 12:44:18 +0530

    layout done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/logo.svg';
+import dashboard from './images/illustration-dashboard.png';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article className='ping-card'>
+      <img src={logo} alt=''/>
+      <h1>We are launching <span>soon!</span></h1>
+      <p>Subscribe and get notified</p>
+      <section className='ping-card-form'>
+        <form noValidate>
+          <input className='pcd' type='email' name='email' placeholder='Your Email Address...'></input>
+          <button className='pcd' type='submit'>Notify Me</button>
+        </form>
+      </section>
+      <section className='ping-card-dashboard'>
+        <img src={dashboard} alt=''/>
+      </section>
+      <footer className='ping-card-copyright'>
+        <section className='icons'>
+          <i class="fa-brands fa-facebook"></i>
+          <i class="fa-brands fa-square-twitter"></i>
+          <i class="fa-brands fa-square-instagram"></i>
+        </section>
+        <p><sup>&copy;</sup>Copyright Ping. All rights reserved.</p>
+      </footer>
+    </article>
   );
 }
 
